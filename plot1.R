11c106be739c5f37d9662531fcc08b84aaac144e
servetd <servetdemirtr27@gmail.com>
2019-08-30 13:44:02 +0300

    revised

--- plot1.R
+++ plot1.R
@@ -5,5 +5,6 @@ epc <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
 sub_epc <- subset(epc, Date=="1/2/2007"|Date=="2/2/2007")
 # Global_active_power is factor so that   data is converted firstly charecter and then to numeric
 hist(as.numeric(as.character(sub_epc$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
-dev.copy(png,"pilot1.png")
-png(filename ="pilot1.png", width = 480, height = 480 )
+dev.copy(png,"plot1.png")
+png(filename ="plot1.png", width = 480, height = 480 )
+dev.off()
--- plot2.R
+++ plot2.R
@@ -9,13 +9,10 @@ sub_epc$Time <- paste (sub_epc$Date, sub_epc$Time)
 sub_epc$Date <-as.Date(sub_epc$Date, format="%d/%m/%Y")
 sub_epc$Time <- strptime(sub_epc$Time, format="%d/%m/%Y %H:%M:%S")
 # if You use local language, you should use this "Sys.setlocale("LC_TIME","en_US.UTF-8")" for convert weekdays in English
-## plot graphs
-plot(sub_epc$Time,sub_epc$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
-with(sub_epc,lines(Time,as.numeric(as.character(Sub_metering_1))))
-with(sub_epc,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
-with(sub_epc,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
-legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+# ploting
+plot(sub_epc$Time,as.numeric(as.character(sub_epc$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 
 #save file
-dev.copy(png,"pilot3.png")
-png(filename ="pilot3.png", width = 480, height = 480 )
+dev.copy(png,"pilot2.png")
+png(filename ="pilot2.png", width = 480, height = 480 )
+dev.off()
